2fe04bd4742e329336f467c4eb1c8439dec1112e
neon97 <dc.vishwakarma.raj@gmail.com>
2019-11-19 22:23:11 +0530

    we have made blogpage =>homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'list.dart';
 
 class Homepage extends StatefulWidget {
   @override
@@ -13,15 +14,83 @@ class _HomepageState extends State<Homepage> {
         title: Text("Hii, Raj"),
         centerTitle: true,
       ),
-      
+
+//asume it we have 10 blogs.....
 
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Center(
-          child: Text("Hello Raj, Welcome you have authenticated yourself !!"),
+        child: ListView(
+          //listview helps us to make a scrollable widgets....
+          //listview and listview.builder
+          children: <Widget>[for (int i = 0; i < 5; i++) texting(context, i)],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrange,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          print("You can add your blogs here !!!");
+        },
+      ),
+    );
+  }
+
+  //ending of stateless widget   it only works for UI itself
+
+  Widget texting(BuildContext context, int index) {
+    double deviceHeight = MediaQuery.of(context).size.height / 3;
+    return Padding(
+      padding: EdgeInsets.fromLTRB(5, 0, 5, 5),
+      child: Card(
+        elevation: 5.0,
+        child: Container(
+          height: deviceHeight,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                height: deviceHeight / 1.35,
+                width: MediaQuery.of(context).size.width,
+                child: FadeInImage(
+                  image: NetworkImage(blogimage[index]),
+                  placeholder: AssetImage("image/logo.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                height: deviceHeight / 4,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Text(
+                      item[index],
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                    Divider(
+                      color: Colors.transparent,
+                      height: 3.0,
+                    ),
+                    Text(
+                      blogsubtitle[index],
+                      style: TextStyle(fontSize: 16.0),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
-}
+} //ending statefull widget
---
+++ lib/list.dart
@@ -0,0 +1,33 @@
+  List<String> blogimage = [
+    "https://hackernoon.com/hn-images/1*s5txnmFryw_DkCmIk6zrhw.jpeg",
+    "https://qbittechnologies.com/wp-content/uploads/2017/02/tech-blog-banner.jpg",
+    "https://cdn.techjuice.pk/wp-content/uploads/2015/01/blog-first-person.jpg",
+    "https://www.fablesquare.com/wp-content/uploads/2016/06/book-workplace.jpg",
+    "https://soundsandcolours.com/static/2019/07/writing.jpg",
+    "https://cdn8.dissolve.com/p/D985_56_092/D985_56_092_1200.jpg",
+    "http://limousin-tech.org/wp-content/uploads/2019/08/1554801935506onlineprogrammingcourses.jpeg",
+    "https://bloggingx.com/wp-content/uploads/2019/01/tech-blog.png",
+    "https://clickup.com/blog/wp-content/uploads/2018/07/tech-blogs-1400x1050.png"
+  ];
+
+  List item = [
+  "Blog",
+  "Macbook",
+  "Application",
+  "Strategy",
+  "Analytics",
+  "Currency"
+];
+
+
+List<String> blogsubtitle = [
+  "Macbook Air hits 5 million sales",
+  "Coding & coffee helps to build login",
+  "Raise in Android Devlopers",
+  "Rectifying issues are bug solver",
+  "Having a flow solves problem !!",
+  "Making money with Online Market",
+  "Programming Languages for 2019",
+  "What Youtubers Earn ??",
+  "Robotics Start AI & ML !!"
+];
