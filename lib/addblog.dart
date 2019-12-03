5f81807011d65a5d83195d8a09e06cf5380dcbd6
neon97 <dc.vishwakarma.raj@gmail.com>
2019-12-03 21:44:29 +0530

    done with the blogpage

--- lib/addblog.dart
+++ lib/addblog.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/homepage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-
-
 
 class ImagePost extends StatefulWidget {
   @override
@@ -10,14 +9,17 @@ class ImagePost extends StatefulWidget {
 }
 
 class _ImagePostState extends State<ImagePost> {
-
 //Image Picker
   File _image;
 
   Future getImage() async {
-    var image = await ImagePicker.pickImage(                                //you can change the source of image by Gallery/Camera
-        source: ImageSource.camera, maxHeight: 800, maxWidth: 800);        //i have define the dimension so as to compress the image....while uploading
-        //source: ImageSource.camera, maxHeight: 800, maxWidth: 800);     
+    var image = await ImagePicker.pickImage(
+        //you can change the source of image by Gallery/Camera
+        source: ImageSource.camera,
+        maxHeight: 800,
+        maxWidth:
+            800); //i have define the dimension so as to compress the image....while uploading
+    //source: ImageSource.camera, maxHeight: 800, maxWidth: 800);
 
     setState(() {
       _image = image;
@@ -27,7 +29,7 @@ class _ImagePostState extends State<ImagePost> {
   @override
   Widget build(BuildContext context) {
     var container = Container(
-        height: 257,                              //defineing the container fopr the image
+        height: 257, //defineing the container fopr the image
         width: 400,
         child: _image == null
             ? DecoratedBox(
@@ -38,7 +40,7 @@ class _ImagePostState extends State<ImagePost> {
                       child: Icon(
                         Icons.camera,
                         size: 50.0,
-                        color: Colors.black,
+                        color: Colors.deepOrange,
                       ),
                       onTap: getImage,
                     ),
@@ -50,6 +52,7 @@ class _ImagePostState extends State<ImagePost> {
                       "Upload Pic from Gallery!",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
+                        color: Colors.deepOrange,
                         fontSize: 14.0,
                       ),
                     )
@@ -66,7 +69,8 @@ class _ImagePostState extends State<ImagePost> {
                       width: 400,
                       child: Image.file(
                         _image,
-                        fit: BoxFit.cover,      //fitting the image to cover the box(Container defined)
+                        fit: BoxFit
+                            .cover, //fitting the image to cover the box(Container defined)
                       ),
                     )
                   ],
@@ -77,29 +81,84 @@ class _ImagePostState extends State<ImagePost> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Image !!"),
-        backgroundColor: Colors.black,
+        title: Text("Add Blog !!"),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange,
       ),
       body: Container(
-        height: MediaQuery.of(context).size.height,     //using mediaquery defines that it will automatically adjust 
-        width: MediaQuery.of(context).size.width,       //according to the device screen size whether its big or small 
+        height: MediaQuery.of(context)
+            .size
+            .height, //using mediaquery defines that it will automatically adjust
+        width: MediaQuery.of(context)
+            .size
+            .width, //according to the device screen size whether its big or small
         padding: EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: ListView(
+          //listview provide a scrollable widget not the column nor the row provides you the same..
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Divider(
-              color: Colors.transparent,
-              height: 15,
-            ),
-
 // Image Box
             GestureDetector(
               child: container,
-              onTap: getImage,        //incase you want to change the other image....
+              onTap: getImage, //incase you want to change the other image....
             ),
+
+            Padding(
+              padding: EdgeInsets.fromLTRB(0.0, 10.0, 100.0, 10.0),
+              child: TextField(
+                controller: title,
+                style: TextStyle(fontSize: 20.0, color: Colors.deepOrange),
+                decoration: InputDecoration(
+                    hintText: "Title",
+                    hintStyle:
+                        TextStyle(color: Colors.deepOrange, fontSize: 20.0)),
+              ),),
+
+              TextField(
+                controller: subtitle,
+                style: TextStyle(fontSize: 18.0, color: Colors.deepOrange),
+                decoration: InputDecoration(
+                    hintText: "Subtitle",
+                    hintStyle:
+                        TextStyle(color: Colors.deepOrange, fontSize: 18.0)),
+              ),
+
+              Divider(color: Colors.transparent,height: 15.0,),
+
+              TextField(
+                controller: details,
+                maxLines: 5,
+                style: TextStyle(fontSize: 18.0, color: Colors.deepOrange),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0)
+                  ),
+                    hintText: "Details",
+                    hintStyle:
+                        TextStyle(color: Colors.deepOrange, fontSize: 18.0)),
+              ),
+            
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.send),
+        backgroundColor: Colors.deepOrange,
+        onPressed: ()
+        {
+          if(_image == null || title.text == "" || subtitle.text == "" || details.text.isEmpty)
+          {
+            print("sir you have left the fields empty");
+          }else{
+            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Homepage()));
+          }
+          
+        },
+      ),
     );
   }
+
+  final title = TextEditingController();
+  final subtitle = TextEditingController();
+  final details = TextEditingController();
 }
